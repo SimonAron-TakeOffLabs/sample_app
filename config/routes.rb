e47726598756eb6f7e7d4a508ef09fda687ed6e5
Aron Simon <aron.simon@takeofflabs.com>
2024-10-04 12:08:16 +0300

    finish signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get '/help' => 'static_pages#help'
   get '/about' => 'static_pages#about'
   get '/signup' => 'users#new'
+
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
