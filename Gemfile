185982233dff78c1d766549213d80b9a5236c7ec
Aron Simon <aron.simon@takeofflabs.com>
2024-10-07 12:08:37 +0300

    add authorization and admins

--- Gemfile
+++ Gemfile
@@ -24,6 +24,11 @@ gem 'bcrypt', '~> 3.1.7'
 gem 'rails-controller-testing'
 gem 'bootstrap-sass', '3.4.1'
 
+gem 'faker', '2.1.2'
+
+gem 'will_paginate', '3.1.8'
+gem 'bootstrap-will_paginate', '1.0.0'
+
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -34,4 +34,14 @@ module SessionsHelper
       end
     end
   end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url) 
+  end
+      # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
 end
